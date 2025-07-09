ee487850e411fbd8a913a149f78935b2df81bbad
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-09 20:11:55 +0900

    feat: add CLI entry point with argument parsing

---
+++ tidyg/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from tidyg import functions
+
+def main():
+    args = parse_args()
+    function = functions[args.function]
+    try:
+        function(*args.args)
+    except TypeError as e:
+        print(e)
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("function", choices=functions)
+    parser.add_argument("args", type=str, nargs="*")
+    args = parser.parse_args()
+    return args
+
+functions = {}
+
+if __name__ == "__main__":
+    main()
